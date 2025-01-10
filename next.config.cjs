b56bfa527fd11e93b4f3e455f4f34ccff8da1337
Mehdi Salimi <Salimi.devop@gmail.com>
2025-01-10 03:41:42 +0330

    chore: initial commit
    - Project initialized
    - Essential packages installed
    - Set up base project architecture
    - Configured:
      - Husky
      - ESLint
      - Prettier
      - Other necessary tools and configurations

---
+++ next.config.cjs
@@ -0,0 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     /* config options here */
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  swcMinify: true,
+  experimental: {
+      polyfillsOptimization: true
+  }
+};
+
+module.exports = nextConfig;
