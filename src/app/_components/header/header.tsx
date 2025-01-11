7a2fcb79f20037705714a59227cf20fa5ea622f2
Mehdi Salimi <Salimi.devop@gmail.com>
2025-01-11 03:45:29 +0330

    feat: add header icons and fix styles

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,16 +1,29 @@
 'use client';
 
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+
 export const Header: React.FC = () => {
     return (
         <header className='flex h-12 w-full justify-between bg-primary px-4 text-text-light'>
             <div className='flex items-center justify-center gap-4'>
-                <h2>logo</h2>
-                <h3>resume scored</h3>
+                <h2 className='font-semibold'>CV Builder</h2>
             </div>
 
             <div className='flex items-center justify-center gap-4'>
-                <button>Download</button>
-                <span>user avatar</span>
+                <Button
+                    size='small'
+                    component='label'
+                    role={undefined}
+                    variant='contained'
+                    tabIndex={-1}
+                    startIcon={<CloudDownloadIcon />}>
+                    Download
+                </Button>
+                <div>
+                    <Avatar alt='Travis Howard' src='https://i.pravatar.cc/150?u=a04258114e29026302d' />
+                </div>
             </div>
         </header>
     );
