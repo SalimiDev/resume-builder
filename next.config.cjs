80250dce53538d2e3c3d709161a05f229715526c
Mehdi Salimi <Salimi.devop@gmail.com>
2025-03-13 01:37:08 +0330

    fix: update next.config.cjs and package.json for output configuration and remove predeploy script

--- next.config.cjs
+++ next.config.cjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     swcMinify: true,
+    output: 'export',
     experimental: {
         polyfillsOptimization: true
-    },
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/resume-builder' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/resume-builder/' : ''
+    }
 };
 
 module.exports = nextConfig;
